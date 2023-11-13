ad164cbf47b4c9a3c8184c762a7797090ab9c921
jord97@korea.edu <jord97@korea.edu>
2023-11-13 20:12:01 +0900

    3트

--- planet_CMS.py
+++ planet_CMS.py
@@ -4,32 +4,32 @@ from mathutils import Vector
 #g =3.
 #G = 6.6740831e-11
 
-class planet(bpy.ops.mesh):
+class Planet:
     def __init__(self, radius, location, velocity):
-        self.object = bpy.ops.mesh.primitive_uv_sphere_add(radius = radius)
+        bpy.ops.mesh.primitive_uv_sphere_add(radius = radius)
+        self.object = bpy.context.active_object
         self.object.location = location
         self.velocity = velocity
-        self.__mass = 10**radius
+        self.mass = 10**radius
         
-    def update(self, location, velocity):
-        self.object.location = location
-        self.velocity = velocity
+    def update(self, acc, rate):
+        self.velocity =- acc * rate
+        self.location =+ i.velocity * rate
 
 
-
-class system(planet):
+class system(Planet):
     def __init__(self, sys, frame_rate):
         self.system = sys
         self.frame_rate = frame_rate
         self.seconds_per_frame = 1 / frame_rate
-        self.__G = 6.6740831e-11
+        self.G = 6.6740831e-11
 
     def update(self):
         for i in self.system:
             acc = Vector(0,0,0)
             for j in self.system.remove(i):
                 acc =+ self.acc(j)
-            i.velocity =- acc * self.seconds_per_frame
+            i.update(acc, self.seconds_per_frame)
         return self.system
     
     def acc(self, other_planet):
@@ -41,17 +41,15 @@ class system(planet):
 
 for i in range(5):
     orbit = []
-    orbit.append(planet(radius=rand.randrange(1,3), 
-                     location=Vector(rand.randrange(-10,10),rand.randrange(-10,10),rand.randrange(-10,10)), 
-                     velocity=Vector(rand.randrange(-1,1),rand.randrange(-1,1), rand.randrange(-1,1))))
+    orbit.append(Planet(radius=rand.randrange(1,3), 
+                     location=Vector((rand.randrange(-10,10),rand.randrange(-10,10),rand.randrange(-10,10))), 
+                     velocity=Vector((rand.randrange(-1,1),rand.randrange(-1,1), rand.randrange(-1,1)))))
 
 sys = system(orbit,1)
 
 def n_bodies(scene):
     global sys
-    planets =[]
-    planets = sys.update()
-    planets
+    sys.update()
 
 
 #get rid of previously set frame_change_pre handlers (if not the same handler may be fired n times)
