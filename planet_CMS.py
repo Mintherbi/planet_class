354a0a99d59666ecd872cc569f9adea6d3c02620
jord97@korea.edu <jord97@korea.edu>
2023-11-15 12:45:21 +0900

    매서드 간단하게 수정

--- planet_CMS.py
+++ planet_CMS.py
@@ -1,59 +1,52 @@
 import bpy
-import random as rand 
+import random as rand
 from mathutils import Vector
-#g =3.
-#G = 6.6740831e-11
 
+G = 6.6740831e-11
+#new code
 class Planet:
     def __init__(self, radius, location, velocity):
-        bpy.ops.mesh.primitive_uv_sphere_add(radius = radius)
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=radius)
         self.object = bpy.context.active_object
         self.object.location = location
         self.velocity = velocity
-        self.mass = 10**radius
-        
-    def update(self, acc, rate):
-        self.velocity -= acc * rate
-        self.location += self.velocity * rate
+        self.mass = 1000**radius
 
 
-class system(Planet):
-    def __init__(self, sys, frame_rate):
-        self.system = sys
+class System:
+    def __init__(self, sys=None, frame_rate=1):
+        self.system = sys if sys else []
         self.frame_rate = frame_rate
         self.seconds_per_frame = 1 / frame_rate
-        self.G = 6.6740831e-11
 
     def update(self):
         for i in self.system:
-            acc = Vector((0,0,0))
-            for j in self.system.pop(i):
-                acc += self.acc(self.system[j])
-            self.system[i].update(acc, self.seconds_per_frame)
-    
-    def acc(self, other_planet):
-        direction = other_planet.object.location - self.object.location
-        d_squared = direction.length_squared
-        direction.normalize()
-        acc = direction * (self.G * other_planet.mass/ d_squared) 
-        return acc
+            acc = Vector((0, 0, 0))
+            for j in self.system:
+                if i != j : 
+                    direction = j.object.location - i.object.location
+                    d_squared = direction.length_squared
+                    direction.normalize()
+                    acc_j = direction * (G * j.mass / d_squared)
+                    acc += acc_j
+            i.velocity += self.frame_rate * acc
+            i.object.location += i.velocity * self.frame_rate
+        return self.system
 
-orbit = []
 
-for i in range(3):
-    orbit.append(Planet(radius=rand.randrange(1,3), 
-                     location=Vector((rand.randrange(-10,10),rand.randrange(-10,10),rand.randrange(-10,10))), 
-                     velocity=Vector((rand.randrange(-1,1),rand.randrange(-1,1), rand.randrange(-1,1)))))
+# Create a system with 5 planets
+orbit = [Planet(radius=rand.randrange(2, 5),
+                location=Vector((rand.randrange(-20, 20), rand.randrange(-20, 20), rand.randrange(-20, 20))),
+                velocity=Vector((rand.random()*2-1, rand.random()*2-1, rand.random()*2-1)))
+         for _ in range(3)]
 
-sys = system(orbit,1)
+sys = System(orbit, 1)
 
 def n_bodies(scene):
     global sys
     sys.update()
-    sys.system
 
-
-#get rid of previously set frame_change_pre handlers (if not the same handler may be fired n times)
+# Clear previously set frame_change_pre handlers
 bpy.app.handlers.frame_change_pre.clear()
-#install "two_bodies" as current handler
+# Install "n_bodies" as the current handler
 bpy.app.handlers.frame_change_pre.append(n_bodies)
